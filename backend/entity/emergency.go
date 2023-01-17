6aaa2c2cd2c5442df9cd923f70dd66652bfe1b3e
Thirawat Eusirwong <torguze@rgmail.com>
2023-01-17 16:45:30 +0700

    entity emergency - close #32

---
+++ backend/entity/emergency.go
@@ -0,0 +1,42 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Emergency struct {
+	gorm.Model
+	Name string
+
+	//For Link Foreign key
+	Case []Case 		`gorm:"foreignKey:EmergencyID"`
+}
+
+type Gender struct {
+	gorm.Model
+	Name string
+
+	//For Link Foreign key
+	Case []Case 		`gorm:"foreignKey:GenderID"`
+}
+
+type Case struct {
+	gorm.Model
+	Location 	string
+	Patient 	string
+	Age			int
+	Status		string
+	Datetime	time.Time
+
+	// Save Emergency area ID in FK
+	EmergencyID *uint
+	// to eaiser for add FK
+	Emergency Emergency
+
+	// Save Gender area ID in FK
+	GenderID *uint
+	// to eaiser for add FK
+	Gender Gender
+}
