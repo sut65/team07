c8057701227b79d81b863c4b97dc6293e2119a6b
B6217174 <B6217174@g.sut.ac.th>
2023-01-30 23:32:13 +0700

    Add Validation AmountDisinfectant - close #221

--- backend/entity/disinfection_validation_test.go
+++ backend/entity/disinfection_validation_test.go
@@ -54,33 +54,33 @@ import (
 // 	g.Expect(err.Error()).To(Equal("Note cannot be blank"))
 // }
 
-// func TestAmountDisintantNotZero(t *testing.T) {
-// 	g := NewGomegaWithT(t)
+func TestAmountDisintantNotZero(t *testing.T) {
+	g := NewGomegaWithT(t)
 
-// 	fixture := []int{
-// 		0, 10000,
-// 	}
+	fixture := []int{
+		0, 10000,
+	}
 
-// 	for _, amount := range fixture {
-// 		disin := Disinfection{
-// 			Note: "ยางรั่ว", 
-// 			AmountDisinfectant: amount, //ผิด
-// 			WorkTime: time.Now().Add(24 * time.Hour),
-// 		}
+	for _, amount := range fixture {
+		disin := Disinfection{
+			Note: "ยางรั่ว", 
+			AmountDisinfectant: amount, //ผิด
+			WorkTime: time.Now().Add(24 * time.Hour),
+		}
 
-// 		//ตรวจสอบด้วย govalidator
-// 		ok, err := govalidator.ValidateStruct(disin)
+		//ตรวจสอบด้วย govalidator
+		ok, err := govalidator.ValidateStruct(disin)
 
-// 		//ok ต้องไม่เป็นค่า true แปลว่าต้องจับ err ได้
-// 		g.Expect(ok).ToNot(BeTrue())
+		//ok ต้องไม่เป็นค่า true แปลว่าต้องจับ err ได้
+		g.Expect(ok).ToNot(BeTrue())
 
-// 		// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
-// 		g.Expect(err).ToNot(BeNil())
+		// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+		g.Expect(err).ToNot(BeNil())
 
-// 		// err.Error ต้องมี error message แสดงออกมา
-// 		g.Expect(err.Error()).To(Equal("The value must be in the range 1-9999"))
-// 	}
-// }
+		// err.Error ต้องมี error message แสดงออกมา
+		g.Expect(err.Error()).To(Equal("The value must be in the range 1-9999"))
+	}
+}
 
 func TestDateNotPast(t *testing.T) {
 	g := NewGomegaWithT(t)
